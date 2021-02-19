7880a148d5df1348823f44f00e3fd0b34b1e2ace
gwanghui-lee <gwanghui.lee@samsung.com>
2021-02-19 19:26:09 +0900

    불필요한 test 제거 - testDifferentClassEquality제거 : 여러개의 하위 class가 있을 때 의미가 있음. - testFrancMultiplication제거 : testMultiplication으로 충분 - testEquality 정리: dollar와 franc에 대한 중복테스트는 정리.

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -5,16 +5,10 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
     @Test
     @DisplayName("test multiplication")
-    void testDollarMultiplication() {
+    void testMultiplication() {
         Money five = Money.dollar(5);
         Assertions.assertEquals(Money.dollar(10), five.times(2));
         Assertions.assertEquals(Money.dollar(15), five.times(3));
-    }
-
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        Assertions.assertEquals(Money.franc(10), five.times(2));
-        Assertions.assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
@@ -22,8 +16,6 @@ class MoneyTest {
     void testEquality() {
         Assertions.assertEquals(Money.dollar(5), Money.dollar(5));
         Assertions.assertNotEquals(Money.dollar(5), Money.dollar(6));
-        Assertions.assertEquals(Money.franc(5), Money.franc(5));
-        Assertions.assertNotEquals(Money.franc(5), Money.franc(6));
         Assertions.assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
@@ -31,11 +23,5 @@ class MoneyTest {
     void testCurrency() {
         Assertions.assertEquals("USD", Money.dollar(1).currency());
         Assertions.assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        Assertions.assertEquals(new Money(10, "CHF"),
-                new Franc(10, "CHF"));
     }
 }
