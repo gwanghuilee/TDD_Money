6e64986c988763edd5bb05cf23a764bf6231a35e
gwanghui-lee <gwanghui.lee@samsung.com>
2021-02-21 01:24:04 +0900

    HashMap을 이용해 rate 저장 및 가져오기 구현

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,12 +1,19 @@
+import java.util.HashMap;
+
 public class Bank {
+    private HashMap rates = new HashMap<Pair, Integer>();
+
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
     void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate;
     }
 }
